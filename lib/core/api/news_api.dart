797f85a0e9576fbe8d2a70dfd1294ca28aa9293f
Niki Rahmadi Wiharto <nikirahmadiwiharto@gmail.com>
2020-09-06 14:15:37 +0700

    update news

--- lib/core/api/news_api.dart
+++ lib/core/api/news_api.dart
@@ -8,9 +8,10 @@ class NewsApi extends MainApi {
   Future<List<NewsModel>> getData() async {
     try {
       final response = await getRequest(
-        url: "http://script.google.com/macros/s/AKfycbxS4eCKGo--UbuEyXorXpxUZU9nh9l7zoDt5Dpyx1cqZ1zVRK4/exec?id=12LdzZxoBz0OIN9jnCQ3RVum8Ce_J1Fw088X8cDAlHDo&sheet=news",
+        url: "$host/news",
+        useAuth: true
       );
-      return newsModelFromMap(jsonEncode(jsonDecode(response)['data']));
+      return newsModelFromMap(jsonEncode(jsonDecode(response)));
     } catch (error) {
       throw Exception(error);
     }
